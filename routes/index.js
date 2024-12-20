1405c3085baa111e11b6a0cb0fa366e42959639b
Jeffrey Doyle <doylejeff98@gmail.com>
2024-12-20 13:12:04 -0500

    added delete button for messages

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-
+const { v4: uuidv4 } = require('uuid'); // Add this line to import uuid
 let messages = [];
 
 router.get("/", (req, res) => {
@@ -13,7 +13,13 @@ router.get("/new", (req, res) => {
 
 router.post("/new", (req, res) => {
     const { user, text } = req.body;
-    messages.push({ user, text, added: new Date() });
+    messages.push({ id: uuidv4(), user, text, added: new Date() });
+    res.redirect("/");
+});
+
+router.post("/delete/:id", (req, res) => {
+    const { id } = req.params;
+    messages = messages.filter(message => message.id !== id);
     res.redirect("/");
 });
 
